b71b205dbacad6642dca5316bb14237e3a7cc1ad
JoaoGST <155569420+JoaoGST@users.noreply.github.com>
2024-09-07 17:50:50 -0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,47 @@
+function pesquisar() {
+    // Obtém a seção onde os resultados da pesquisa serão exibidos
+    let section = document.getElementById("resultados-pesquisa");
+
+    let campoPesquisa = document.getElementById("campo-pesquisa").value;
+    console.log(campoPesquisa)
+
+    if(!campoPesquisa){
+        section.innerHTML = "<p>Nenhum resultado encontrado. Você precisa digitar um valor.</p>"
+        return;
+    }
+
+    campoPesquisa = campoPesquisa.toLowerCase()
+
+    // Inicializa uma string vazia para armazenar os resultados formatados
+    let resultados = "";
+    let titulo = "";
+    let descricao = "";
+    let tags = "";
+
+    // Itera sobre cada dado na lista de dados
+    for (let dado of dados) {
+        titulo = dado.titulo.toLowerCase()
+        descricao = dado.descricao.toLowerCase()
+        tags = dado.tags.toLowerCase()
+
+        if (titulo.includes(campoPesquisa) || descricao.includes(campoPesquisa) || tags.includes(campoPesquisa)){
+            // Concatena HTML para criar um novo item de resultado
+            resultados += `
+            <div class="item-resultado">
+                <h2>
+                    <a href="#" target="_blank">${dado.titulo}</a>
+                </h2>
+                <p class="descricao-meta">${dado.descricao}</p>
+                <a href="${dado.link}" target="_blank">Mais informações</a>
+            </div>
+        `;
+        }
+    }
+
+    if(!resultados) {
+        resultados = "<p>Nenhum resultado encontrado.</p>"
+    }
+
+    // Atribui o HTML gerado à seção de resultados
+    section.innerHTML = resultados;
+}
---
+++ dados.js
@@ -0,0 +1,39 @@
+let dados = [
+    {
+        titulo: "Rayssa Leal",
+        descricao: "Rayssa Leal, a Fadinha, é uma skatista brasileira que conquistou o mundo com seu talento e carisma. Com apenas 16 anos, ela já é considerada uma das maiores skatistas de todos os tempos.",
+        link: "https://pt.wikipedia.org/wiki/Rayssa_Leal",
+        tags: "skate fada sk8"
+    },
+    {
+        titulo: "Beatriz Souza",
+        descricao: "Beatriz Souza é uma judoca brasileira que se destaca pela sua técnica e determinação. Ela já conquistou diversas medalhas em competições nacionais e internacionais.",
+        link: "https://pt.wikipedia.org/wiki/Beatriz_Souza",
+        tags: "judô judo judoka"
+    },
+    {
+        titulo: "Rebeca Andrade",
+        descricao: "Rebeca Andrade é uma ginasta brasileira que encantou o mundo com suas acrobacias e beleza. Ela foi a primeira ginasta brasileira a conquistar uma medalha de ouro olímpica.",
+        link: "https://pt.wikipedia.org/wiki/Rebeca_Andrade",
+        tags: "ginastica ginástica ouro"
+    }
+];
+
+const instrumentos = [
+    {
+      nome: "Violão",
+      descricao: "Instrumento de cordas, muito versátil, utilizado para acompanhar canções, solos e harmonias. Existem diversos tipos de violão, como o clássico, o folk e o elétrico.",
+      generos: ["MPB", "Rock", "Pop", "Sertanejo", "Blues"]
+    },
+    {
+      nome: "Bateria",
+      descricao: "Conjunto de percussão composto por diversos tambores e pratos, responsável pela marcação do ritmo em diversas músicas. É fundamental para gêneros como rock, pop e jazz.",
+      generos: ["Rock", "Pop", "Jazz", "Samba", "Funk"]
+    },
+    {
+      nome: "Piano",
+      descricao: "Instrumento de teclado com 88 teclas, capaz de produzir uma ampla gama de sons. É utilizado em diversos estilos musicais, desde clássicos até músicas mais modernas.",
+      generos: ["Clássico", "Jazz", "Pop", "Rock", "Blues"]
+    }
+  ];
+  
